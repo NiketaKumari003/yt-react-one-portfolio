1f4a3998d5bbf668c8ba5011f67209412936c4aa
delta-demo <niketa78709846@gmail.com>
2024-06-22 21:11:52 +0530

    project done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { FaSignature } = require('react-icons/fa');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily:{
+      signature:["Great Vibes"],
+    }
+  },
+  plugins: [],
+}
